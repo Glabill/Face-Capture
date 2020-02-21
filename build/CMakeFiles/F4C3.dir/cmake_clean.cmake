55ad025faa15a182b3ab4fabd2faf2db7d7be478
Glabill <gaston.gougeon@gmail.com>
2020-02-21 13:37:51 -0500

    Next 21.02.2020

---
+++ build/CMakeFiles/F4C3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/F4C3.dir/main.cpp.o"
+  "F4C3.pdb"
+  "F4C3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/F4C3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/F4C3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for F4C3.
+# This may be replaced when dependencies are built.
